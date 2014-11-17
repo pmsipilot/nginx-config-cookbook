1154138f3d5e4b698bf38ef332d46cd371d5934a
jubianchi <julien.bianchi@pmsipilot.com>
2014-11-17 18:01:28 +0100

    Création du lien symbolic dans le dossier des sites activés

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,8 +3,18 @@ service 'nginx' do
   action :nothing
 end
 
-
-template '/etc/nginx/sites-available/proxy.conf' do
+template 'configure_server' do
   source 'nginx.conf.erb'
+  path '/etc/nginx/sites-available/proxy.conf'
+
+  notifies :create, 'link[enable_server]', :immediately
+  notifies :restart, 'service[nginx]', :delayed
+end
+
+link 'enable_server' do
+  target_file '/etc/nginx/sites-enabled/proxy.conf'
+  to '/etc/nginx/sites-available/proxy.conf'
+  action :nothing
+
   notifies :restart, 'service[nginx]', :delayed
 end
