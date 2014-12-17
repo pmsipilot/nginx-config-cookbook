01505181904d56b47a1ed8546a97a40f2d77f051
jubianchi <julien.bianchi@pmsipilot.com>
2014-12-17 11:39:11 +0100

    Update long description

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer       'PMSIpilot'
 maintainer_email 'support@pmsipilot.com'
 license          'MIT'
 description      'Configures nginx'
-long_description 'Configures nginx'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
